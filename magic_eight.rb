44a4063d0c93405adb59c80e2bb2009add185a34
Jake Bills <jakebills1@gmail.com>
2019-02-14 14:36:23 -0700

    add menu

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,23 +1,46 @@
 require "pry"
+require "colorize"
 
 class EightBall
-    def initialize(answers)
-        @answers = answers
+    def initialize(answers, options)
+        @answers, @options = answers, options
         question
     end
     def question
         puts "Welcome to the Ruby magic eight ball"
-        puts "Type a question, or type quit to exit"
-        input = gets.strip
-        if input != "quit"
-            puts @answers.sample
+        puts "What would you like to do?"
+        
+        @options.each_with_index do |option, index|
+            puts "#{index + 1}) #{option}"
+        end
+        
+        input = gets.to_i
+        
+        case input
+        when 1
+            puts "Type your question now: "
+            q = gets
+            puts @answers.sample.colorize(:red)
+            puts 
             question
-        else
+        when 2
+            add_answer
+        when 3
+            reset
+        when 4
+            puts "Goodbye"
+            sleep(3)
+            puts `clear`
             exit
+        else
+            puts "invalid entry"
+            question
         end
     end
 end
 
 answers = ["yes", "no", "maybe", "Not likely", "All signs point to no"]
 
-app = EightBall.new(answers)
+menu_options = ["Ask a question", "Add an answer", "Reset to default answers", "quit"]
+
+app = EightBall.new(answers, menu_options)
